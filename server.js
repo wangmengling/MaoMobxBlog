1b671ea0d88e36dad30c19a695f6f00c3be883cd
王国仲 <guo-zhong.wang@utsoft.cn>
2017-05-23 21:41:33 +0800

    first commit
    commit

---
+++ server.js
@@ -0,0 +1,45 @@
+import Static from 'koa-static';
+import Send from 'koa-send';
+import Koa from 'koa';
+import path from 'path';
+const app = new Koa();
+
+// x-response-time
+
+app.use(async function (ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async function (ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
+
+app.use(Static(path.join(__dirname, 'static')));
+app.use(async (ctx) => {
+  await Send(ctx, '/static/index.html');
+})
+app.listen(3001);
