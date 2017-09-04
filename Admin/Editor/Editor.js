4d5a6f09ed9ed0dcd6d6020437540d12b8339ae9
王国仲 <294025529@qq.com>
2017-09-04 23:26:50 +0800

    commit
    commit

--- Admin/Editor/Editor.js
+++ Admin/Editor/Editor.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 // import  ReactMarkdown from 'react-markdown'
 import MarkdownInput from '@opuscapita/react-markdown'
+import PlainMarkdownInput from '@opuscapita/react-markdown'
 var input = '## This is a header\n\n*And this is a paragraph*';
 import './Editor.scss'
 class Editor extends Component {
+    onChangeContent = () => {
+    // this.props.homeState.changeContent();
+    }
     render() {
         return (
             <div className="EditorContent">
                  <div className="left-wrapper">
-                    <MarkdownInput onChange/>
+                    <PlainMarkdownInput
+                        onChange={_scope.handleValueChange}
+                        value={_scope.state.value}
+                    />
                 </div>
                 <div className="right-wrapper">
                     <div className="output">
