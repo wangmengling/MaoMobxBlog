0c56e2df3c3b46b74ccabf7836779fe352a250fa
王国仲 <294025529@qq.com>
2017-09-08 23:07:18 +0800

    commit
    commit

--- Admin/Editor/Editor.js
+++ Admin/Editor/Editor.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react'
-// import  ReactMarkdown from 'react-markdown'
-import MarkdownInput from '@opuscapita/react-markdown'
-import PlainMarkdownInput from '@opuscapita/react-markdown'
+import  ReactMarkdown from 'react-markdown'
+import {MarkdownEditor} from 'react-markdown-editor';
+// import CodeMirror from 'react-codemirror';
+import LzEditor from 'react-lz-editor'
 var input = '## This is a header\n\n*And this is a paragraph*';
 import './Editor.scss'
 class Editor extends Component {
-    onChangeContent = () => {
-    // this.props.homeState.changeContent();
+
+    constructor(props){
+        super(props)
+        this.state = {
+            htmlContent: `<h1>Yankees, Peeking at the Red Sox, Will Soon Get an Eyeful</h1>
+                      <p>Whenever Girardi stole a glance, there was rarely any good news for the Yankees. While Girardi’s charges were clawing their way to a split of their four-game series against the formidable Indians, the Boston Red Sox were plowing past the rebuilding Chicago White Sox, sweeping four games at Fenway Park.</p>`,
+            markdownContent: "## HEAD 2 \n markdown examples \n ``` welcome ```",
+            responseList: []
+          }
+          this.receiveHtml=this.receiveHtml.bind(this);
+   }
+   receiveHtml(content) {
+    console.log("recieved HTML content", content);
+   }
+    onChangeContent(e) {
+        console.log(e)
+        this.setState({content: e});
     }
     render() {
+        var options = {
+            lineNumbers: true,
+        };
         return (
             <div className="EditorContent">
+                
                  <div className="left-wrapper">
-                    <PlainMarkdownInput
-                        onChange={_scope.handleValueChange}
-                        value={_scope.state.value}
-                    />
+                    {/* <MarkdownEditor 
+                        initialContent={this.state.content} 
+                        iconsSet="font-awesome"
+                        onContentChange={(e) => this.onChangeContent(e)}
+                    /> */}
+                    <LzEditor active={true}  importContent={this.state.htmlContent} cbReceiver={this.receiveHtml} 
+        lang="en"/>
+        <br/>
                 </div>
                 <div className="right-wrapper">
-                    <div className="output">
-                        <h1 id="titleOutput"></h1>
-                        <div id="articleOutput"></div>
+                    <div className="showContent">
+                        <div  className="showContentTitle">
+                            测试
+                        </div>
+                        <div  className="showContentContent">
+                            <ReactMarkdown  source={this.state.htmlContent} theme='monokai' mode='markdown' /> 
+                        </div>
                     </div>
+                    
+                    
                 </div>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,12 @@ module.exports = {
       }, {
           test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
           use: ['url-loader?limit=10000&mimetype=image/svg+xml']
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }],
   }
 };
